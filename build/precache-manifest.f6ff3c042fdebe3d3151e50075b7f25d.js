fb1338972c800145a9b6a8fd6da7e7bad352ae91
hasan-py <hasan29351@gmail.com>
2020-12-09 00:30:02 +0600

    Ready to Deploy

---
+++ build/precache-manifest.f6ff3c042fdebe3d3151e50075b7f25d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ad6217baf7b1089182cee4bd25db25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "740ad89c5f6267a31851",
+    "url": "/static/css/main.dc3f69ba.chunk.css"
+  },
+  {
+    "revision": "0e9e2a50e7e09f139806",
+    "url": "/static/js/2.9389179e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9389179e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740ad89c5f6267a31851",
+    "url": "/static/js/main.5482541f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
